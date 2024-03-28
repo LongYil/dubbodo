93194fc9a15dabc03405f5630af16d589a74fff6
liyinlong <liyinlong>
2024-03-28 18:17:38 +0800

    fix-支持consul

--- src/test/java/com/example/dubbodo/Demo.java
+++ src/test/java/com/example/dubbodo/Demo.java
@@ -1,10 +1,10 @@
 package com.example.dubbodo;
 
-import com.alibaba.nacos.api.NacosFactory;
-import com.alibaba.nacos.api.config.ConfigService;
-import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingService;
-import com.alibaba.nacos.api.naming.pojo.Instance;
+//import com.alibaba.nacos.api.NacosFactory;
+//import com.alibaba.nacos.api.config.ConfigService;
+//import com.alibaba.nacos.api.exception.NacosException;
+//import com.alibaba.nacos.api.naming.NamingService;
+//import com.alibaba.nacos.api.naming.pojo.Instance;
 
 import java.util.Properties;
 
@@ -24,18 +24,18 @@ public class Demo {
 //
 //            String content = configService.getConfig(dataId, group, 5000);
 //            System.out.println(content);
-            Properties properties = new Properties();
-            properties.put("serverAddr", "10.10.101.246:32576");
-            properties.put("username", "test");
-            properties.put("password", "test");
-            NamingService namingService = NacosFactory.createNamingService(properties);
-            String serverStatus = namingService.getServerStatus();
-            System.out.println(serverStatus);
-            Instance instance = new Instance();
-            instance.setInstanceId("welcome");
-            instance.setServiceName("sayhello");
-            instance.setIp("10.10.101.140");
-            namingService.registerInstance("user", instance);
+//            Properties properties = new Properties();
+//            properties.put("serverAddr", "10.10.101.246:32576");
+//            properties.put("username", "test");
+//            properties.put("password", "test");
+//            NamingService namingService = NacosFactory.createNamingService(properties);
+//            String serverStatus = namingService.getServerStatus();
+//            System.out.println(serverStatus);
+//            Instance instance = new Instance();
+//            instance.setInstanceId("welcome");
+//            instance.setServiceName("sayhello");
+//            instance.setIp("10.10.101.140");
+//            namingService.registerInstance("user", instance);
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
